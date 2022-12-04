ab1de5df4b1311bb82fa031d4b53da28014b9acd
plotnikov-ep <plotnikov-ep@yandex-team.ru>
2022-12-04 15:19:33 +0100

    add template

--- index.11ty.js
+++ index.11ty.js
@@ -1,30 +1,40 @@
 const { getNotionData } = require("./utils/get-notion-data");
 
-module.exports = async function(data) {
-    const exircises = await getNotionData();
+class Index {
+    data() {
+        return {
+            layout: "layout",
+        };
+    }
 
-    return Object.entries(exircises).map(([exerciseName, amounts]) => {
-        const pieces = [
-            `<h2>${exerciseName}</h2>`,
-            `<p>Начальное количество повторений: ${amounts[0]}</p>`,
-            `<p>Последнее количество повторений: ${amounts[amounts.length - 1]}</p>`,
-            `<p>Относительные изменения:</p>`
-        ];
+    async render(data) {
+        const exircises = await getNotionData();
 
-        for (let i = 1; i < amounts.length; i++) {
-            const prevAmount = amounts[i - 1];
-            const amount = amounts[i];
-            const percent = (amount / prevAmount) * 100 - 100;
+        return Object.entries(exircises).map(([exerciseName, amounts]) => {
+            const pieces = [
+                `<h2>${exerciseName}</h2>`,
+                `<p>Начальное количество повторений: ${amounts[0]}</p>`,
+                `<p>Последнее количество повторений: ${amounts[amounts.length - 1]}</p>`,
+                `<p>Относительные изменения:</p>`
+            ];
 
-            pieces.push(`<span>${percent >= 0 ? '+' : '-'}${percent.toFixed(2)}%</span>`);
-        }
+            for (let i = 1; i < amounts.length; i++) {
+                const prevAmount = amounts[i - 1];
+                const amount = amounts[i];
+                const percent = (amount / prevAmount) * 100 - 100;
 
-        const globalPercent = (amounts[amounts.length - 1] / amounts[0]) * 100 - 100;
+                pieces.push(`<span>${percent >= 0 ? '+' : '-'}${percent.toFixed(2)}%</span>`);
+            }
 
-        pieces.push(
-            `<p>Прогресс за всё время: ${globalPercent >= 0 ? '+' : '-'}${globalPercent.toFixed(2)}%</p>`
-        );
+            const globalPercent = (amounts[amounts.length - 1] / amounts[0]) * 100 - 100;
 
-        return `<div>${pieces.join('')}</div>`
-    }).join('');
-};
+            pieces.push(
+                `<p>Прогресс за всё время: ${globalPercent >= 0 ? '+' : '-'}${globalPercent.toFixed(2)}%</p>`
+            );
+
+            return `<div>${pieces.join('')}</div>`
+        }).join('');
+    }
+}
+
+module.exports = Index;
